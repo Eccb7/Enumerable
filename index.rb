8f265270cd78523221ea00f8c1ef8eb91d816b8e
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-08 13:07:32 +0300

    Fixed code style issues in index.rb

--- index.rb
+++ index.rb
@@ -5,12 +5,12 @@ require_relative 'my_enumerable'
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-puts list.all?({ |e| e < 5 })
-puts list.all?({ |e| e > 5 })
+puts list.all? { |e| e < 5 }
+puts list.all? { |e| e > 5 }
 
 # Test #any?
-puts list.any?({ |e| e == 2 })
-puts list.any?({ |e| e == 5 })
+puts list.any? { |e| e == 2 }
+puts list.any? { |e| e == 5 }
 
 # Test #filter
 puts list.filter(&:even?).inspect
