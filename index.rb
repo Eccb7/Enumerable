6f89299636441aad51e4d4012722f08f1a04a33d
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-08 11:50:40 +0300

    Add tests for #all?, #any?, and #filter methods.

---
+++ index.rb
@@ -0,0 +1,16 @@
+require_relative 'my_list'
+require_relative 'my_enumerable'
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? { |e| e < 5 }
+puts list.all? { |e| e > 5 }
+
+# Test #any?
+puts list.any? { |e| e == 2 }
+puts list.any? { |e| e == 5 }
+
+# Test #filter
+puts list.filter { |e| e.even? }.inspect
